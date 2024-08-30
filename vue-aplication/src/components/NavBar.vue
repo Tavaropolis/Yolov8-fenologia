838275424ee047a6036ad4b3c4a432c7e11cd4f2
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-08-29 21:19:35 -0300

    feature: first version navbar

---
+++ vue-aplication/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav>
+        <div class="nav-bar">
+            <ul>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.nav-bar {
+    width: 100vw;
+    height: 5vh;
+    background: var(--menu-color);
+}
+
+ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+}
+</style>
--- vue-aplication/src/views/MainView.vue
+++ vue-aplication/src/views/MainView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import NavBar from "../components/NavBar.vue"
 </script>
 
 <template>
   <main>
+    <NavBar/>
     <h1>Main page</h1>
   </main>
 </template>
