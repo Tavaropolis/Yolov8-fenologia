8e001abd43adc2c063160daffee09e6acd669600
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-08-29 21:59:34 -0300

    style: changes in style

--- vue-aplication/src/components/NavBar.vue
+++ vue-aplication/src/components/NavBar.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
 
 </script>
 
 <template>
     <nav>
         <div class="nav-bar">
+            <img src="../assets/folha.png" alt="Folha de árvore" class="nav-logo">
             <ul>
-                <li>Teste</li>
-                <li>Teste</li>
-                <li>Teste</li>
+                <li><RouterLink to="/">Home</RouterLink></li>
+                <li><RouterLink to="/sobre">Sobre</RouterLink></li>
             </ul>
         </div>
     </nav>
@@ -18,12 +19,30 @@
 .nav-bar {
     width: 100vw;
     height: 5vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
     background: var(--menu-color);
 }
 
+.nav-logo {
+    width: 40px;
+    height: 40px;
+}
+
 ul {
+    width: 12vw;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     list-style: none;
+}
+
+ul li a {
+    cursor: pointer;
+    color: var( --text-color);
+    text-decoration: none;
 }
 </style>
