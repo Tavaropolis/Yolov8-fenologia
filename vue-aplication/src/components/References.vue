5f650466deccf6b63fda222c7465d87d1a937d07
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-09-08 10:38:51 -0300

    fix: changes in References

--- vue-aplication/src/components/References.vue
+++ vue-aplication/src/components/References.vue
@@ -7,6 +7,7 @@
     <ul>
         <li>FOURNIER, L.A. Un método cuantitativo para la medición de características fenológicas en árboles. <span>Turrialba</span>. v.24, p. 422-423. dez./1974.</li>
         <li>HE, Kaiming; GKIOXARI, Georgia; DOLLÁR, Piotr. Mask R-CNN. <span>IEEE Transactions on Pattern Analysis and Machine Intelligence</span>, v. 42, n. 2, p. 386-397, fev./2021.</li>
+        <li>KATAL, N. et al. Deep Learning in Plant Phenological Research: A Systematic Literature Review. <span>Frontiers in plant science</span>, v. 13, n. 1, p. 1-18, mar./2022. Disponível em: https://doi.org/10.3389/fpls.2022.805738 . Acesso em: 5 set. 2024.</li>
         <li>LIETH, H.<span>Phenology and Seasonality Modeling</span>. 1. ed. Nova York: Springer Science+Business Media, 1974. p. 4-5.</li>
         <li>LOUSSAIEF, Sehla; ABDELKRIM, Afef. Convolutional Neural Network Hyper-Parameters Optimization based on Genetic Algorithms. <span>International Journal of Advanced Computer Science and Applications (IJACSA)</span>, v. 9, n. 10, p. 252-266, 2018. Disponível em: http://dx.doi.org/10.14569/IJACSA.2018.091031. Acesso em: 5 set. 2024.</li>
     </ul>
