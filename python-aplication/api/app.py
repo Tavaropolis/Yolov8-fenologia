3c40fba8ce8d4fdcddb8fcebcd4ad0821298132b
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-09-26 13:20:15 -0300

    refactor: refactor of verel.json and app.py

--- python-aplication/api/app.py
+++ python-aplication/api/app.py
@@ -77,8 +77,6 @@ def image_to_python():
         }), 200)
 
         response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'POST'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
         
         return response
     except: 
